cc93a429ecef0ac1b684f951d6c31aa6b70abd50
Lucas <lucas.amorim.cruz@gmail.com>
2026-09-12 14:18:11 -0300

    arrumando

--- src/server.cpp
+++ src/server.cpp
@@ -1,19 +1,34 @@
 #include "include/server.hpp"
-Server::Server ()
+Server::Server()
 {
-   
 }
-void Server::start(){
+void Server::start()
+{
     this->socket.start();
-    while (RUNNING){
+    while (RUNNING)
+    {
         int client = this->socket.acceptConnection();
         if (client == -1)
         {
             std::println(stderr, "Erro ao aceitar conexão do cliente");
             continue;
         }
-        
-
-    }   
+        char buffer[4096];
+        ssize_t bytesReceived = recv(client, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived == -1)
+        {
+            std::println(stderr, "recv: {}", std::strerror(errno));
+            close(client);
+            continue;
+        }
+        if (bytesReceived == 0)
+        {
+            close(client);
+            continue;
+        }
+        buffer[bytesReceived] = '\0';
+        std::println("Recebido!");
+        std::println("{}", buffer);
+        close(client);
+    }
 }
-
